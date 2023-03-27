7ef8dd86617bd55d57d6745cf6d0113c59425724
parkkyeongtae <parkkt96@naver.com>
2023-03-27 18:14:28 +0900

    데이터베이스 연결 최종본

--- database/src/database2/EmpDAO.java
+++ database/src/database2/EmpDAO.java
@@ -146,4 +146,64 @@ public class EmpDAO {
 		return status;
 	}
 
+	// 사원 삭제
+	public boolean remove(int empno) {
+		boolean status = false;
+		try {
+
+			con = getConnection();
+			// empno
+			String sql = "delete from emp_temp where empno=?";
+
+			// ? 처리
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, empno);
+
+			int result = pstmt.executeUpdate();
+
+			if (result > 0)
+				status = true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+		return status;
+	}
+
+	// 사원 추가
+	public boolean insert(EmpDTO empDTO) {
+		boolean status = false;
+		try {
+
+			con = getConnection();
+			// empno
+			String sql = "insert into emp_temp(empno,ename,job,mgr,hiredate,sal,comm,deptno) ";
+			sql += "values(?,?,?,?,sysdate,?,?,?)";
+
+			// ? 처리
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, empDTO.getEmpno());
+			pstmt.setString(2, empDTO.getEname());
+			pstmt.setString(3, empDTO.getJob());
+			pstmt.setInt(4, empDTO.getMgr());
+			pstmt.setInt(5, empDTO.getSal());
+			pstmt.setInt(6, empDTO.getComm());
+			pstmt.setInt(7, empDTO.getDeptno());
+			
+		
+			int result = pstmt.executeUpdate();
+
+			if (result > 0)
+				status = true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(con, pstmt);
+		}
+		return status;
+	}
+
 }
