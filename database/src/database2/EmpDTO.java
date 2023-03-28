29077fa39ea65c7685c1b132e668e0692cda3965
parkkyeongtae <parkkt96@naver.com>
2023-03-28 18:17:20 +0900

    데이터베이스 수정

--- database/src/database2/EmpDTO.java
+++ database/src/database2/EmpDTO.java
@@ -2,7 +2,6 @@ package database2;
 
 import java.util.Date;
 
-import database.Select;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,15 +9,11 @@ import lombok.Setter;
 import lombok.ToString;
 
 //table 컬럼과 동일하게 작성
-//getter/setter 메소드, 생성자, toString()
+//getter/setter 메소드, 생성자, toString() 작성
 
-@AllArgsConstructor
-@ToString
-@Getter
-@Setter
-@NoArgsConstructor
+@AllArgsConstructor @ToString
+@Getter @Setter @NoArgsConstructor
 public class EmpDTO {
-
 	private int empno;
 	private String ename;
 	private String job;
@@ -26,6 +21,7 @@ public class EmpDTO {
 	private Date hiredate;
 	private int sal;
 	private int comm;
-	private int deptno;
-
+	private int deptno;	
+	
+	
 }
